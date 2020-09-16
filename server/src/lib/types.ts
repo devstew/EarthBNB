72554dc5e4134ff884221da234fe81a70ca54d44
devolper7@gmail.com <fortuna2998>
2020-09-17 01:25:15 +0300

    new fields for listing, user, booking

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,31 +1,61 @@
 import { Collection, ObjectId } from 'mongodb';
 
+export enum ListingType {
+    Apartmemt = 'apartment',
+    House = 'house'
+}
+
+export interface BookingsIndexMonth {
+    [key: string]: boolean;
+}
+
+export interface BookingsIndexYear {
+    [key: string]: BookingsIndexMonth
+}
+
+export interface BookingsIndex {
+    [key: string]: BookingsIndexYear;
+}
+
 export interface Listing {
     _id: ObjectId;
     title: string;
+    description: string;
     image: string;
+    host: string;
+    type: ListingType;
     address: string;
+    country: string;
+    admin: string;
+    city: string;
+    bookings: ObjectId[];
+    bookingsIndex: BookingsIndex;
     price: number;
     numOfGuests: number;
-    numOfBeds: number;
-    numOfBaths: number;
-    rating: number;
+}
+
+export interface Booking {
+    _id: ObjectId;
+    listing: ObjectId;
+    tenant: string;
+    checkIn: string;
+    checkOut: string;
+}
+
+export interface User {
+    _id: string;
+    token: string;
+    name: string;
+    avatar: string;
+    contact: string;
+    walletId?: string;
+    income: number;
+    bookings: ObjectId[];
+    listings: ObjectId[];
 }
 
 export interface Database {
+    bookings: Collection<Booking>;
     listings: Collection<Listing>;
+    users: Collection<User>;
 }
-
-// ----
-
-interface  IdentityObj<T = any> {
-    field: T;
-}
-const identity = <TData = any, TVariables = any>(arg: TData): TData => {
-    const obj: IdentityObj<TData> = {
-        field: arg,
-    }
-    return obj.field;
-}
-
-identity<number>(5);
