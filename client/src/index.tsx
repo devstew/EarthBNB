3d02ed3756f6bbcff305756c637ecbad1de2044c
devolper7@gmail.com <fortuna2998>
2020-09-30 00:27:28 +0300

    google consent auth url redirect

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {
@@ -10,6 +10,7 @@ import {
     NotFound,
     User
 } from './sections';
+import { Viewer } from './lib/types';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
@@ -19,7 +20,16 @@ const client = new ApolloClient({
     uri: "/api",
 });
 
+const initialViewer: Viewer = {
+    id: null,
+    token: null,
+    avatar: null,
+    hasWallet: null,
+    didRequest: false
+}
+
 const App = () => {
+    const [viewer, setViewer] = useState<Viewer>(initialViewer);
     return (
         <Router>
             <Switch>
@@ -27,7 +37,11 @@ const App = () => {
                 <Route exact path="/host" component={Host}/>
                 <Route exact path="/listing:id" component={Listing}/>
                 <Route exact path="/listings:location?" component={Listings}/>
-                <Route exact path="/login" component={Login}/>
+                <Route
+                    exact
+                    path="/login"
+                    render={props => <Login {...props} setViewer={setViewer} />}
+                />
                 <Route exact path="/user:id" component={User}/>
                 <Route component={NotFound}/>
             </Switch>
--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,13 +1,43 @@
 import * as React from 'react';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
 import { Card, Layout, Typography } from "antd";
+import { AUTH_URL} from "../../lib/graphql/queries/AuthUrl";
+import { LOG_IN} from "../../lib/graphql/mutations";
+import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
+import { LogIn as LogInData, LogInVariables } from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { Viewer } from '../../lib/types';
+
 
 //image assets
 import googleLogo from './assets/google_logo.jpg';
 
+interface Props {
+    setViewer: (viewer: Viewer) => void;
+}
+
 const { Content } = Layout;
 const { Text, Title  } = Typography;
 
-export const Login = () => {
+export const Login = ({ setViewer }: Props) => {
+    const client = useApolloClient();
+    const [logIn, {
+        data: logInData,
+        loading: logInLoading,
+        error: logInError
+    }] = useMutation<LogInData, LogInVariables>(LOG_IN);
+
+    const handleAuthorize = async () => {
+        try {
+            const { data } = await client.query<AuthUrlData>({
+                query: AUTH_URL
+            })
+
+            window.location.href = data.authUrl;
+        } catch {
+
+        }
+    };
+
     return (
         <Content className="log-in">
             <Card className="log-in-card">
@@ -22,7 +52,7 @@ export const Login = () => {
                     </Title>
                     <Text> Sign in with Google to start booking available rentals!</Text>
                 </div>
-                <button className="log-in-card__google-button">
+                <button className="log-in-card__google-button" onClick={handleAuthorize}>
                     <img src={googleLogo} alt="Google Logo" className="log-in-card__google-button-logo"/>
                     <span className="log-in-card__google-button-text">
                         Sign in with Google
